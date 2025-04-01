765d9a2ad140baaf60601fdd50261f97560d3dc6
hysk <vectorss03@gmail.com>
2025-04-01 23:36:26 +0900

    implement page-header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        <h1 class="w-full h-full px-6 text-2xl text-left leading-16">P R I S M</h1>
+    <div class="flex w-full h-full px-4 py-2 justify-between">
+        <h1 class="pl-2 text-2xl leading-10">P R I S M</h1>
+
+        <div class="flex ml-auto mr-4 p-2 items-center justify-center rounded-lg shadow-sm bg-teal-100 hover:bg-teal-200">
+            <div class="i-material-symbols:share-outline mr-2 text-xl text-teal-800" />
+            <p class="text-sm text-teal-800">공유</p>
+        </div>
+        <div class="flex mr-4 p-2 items-center justify-center rounded-lg shadow-sm bg-teal-100 hover:bg-teal-200">
+            <div class="i-material-symbols:download-rounded mr-2 text-xl text-teal-800" />
+            <p class="text-sm text-teal-800">저장</p>
+        </div>
+        <div class="flex p-2 items-center justify-center rounded-lg shadow-sm bg-teal-100 hover:bg-teal-200">
+            <div class="i-mdi:presentation-play mr-2 text-xl text-teal-800" />
+            <p class="text-sm text-teal-800">슬라이드 쇼</p>
+        </div>
     </div>
 </template>
--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -7,7 +7,7 @@ import SideBar from '@/components/SideBar.vue';
 
 <template>
     <div class="absolute left-0 top-0 flex flex-col w-screen h-screen overflow-auto select-none">
-        <PageHeader class="max-h-16 border-b border-gray-200" />
+        <PageHeader class="max-h-14 border-b border-gray-200" />
         <div class="flex flex-row min-h-64 flex-auto">
             <SideBar class="flex-1/6 min-w-84 border-r border-gray-200" />
             <div class="relative flex-5/6 overflow-hidden bg-gray-100">
